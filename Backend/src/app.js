6c80b7a5e272bb409366a53d923298fcbb6bee58
ViJaya-kh22 <vijayakhavnekar@gmail.com>
2026-04-25 22:43:58 +0530

    fix: add Vercel URL to CORS whitelist

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 app.use(cookie_parser());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ 
+    origin: ['http://localhost:5173', 'https://vibe-nest-ivory.vercel.app'],
+     credentials: true }));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/music' , musicRoutes);
