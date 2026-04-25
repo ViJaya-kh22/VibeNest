599314e5a0714fa6a663ea28c6fb2d88f82ab696
ViJaya-kh22 <vijayakhavnekar@gmail.com>
2026-04-25 23:19:41 +0530

    fix: cookie options for cross-domain auth

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,99 +1,101 @@
-const userModel = require('../models/user.model')
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
+const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
-async function registerUser(req,res) {
-    
-    try {
-       
-     const {username , email , password ,role = 'user'} = req.body;
+async function registerUser(req, res) {
+  try {
+    const { username, email, password, role = "user" } = req.body;
 
-   const isUserAlreadyExist = await userModel.findOne({
-     $or : [
-        {username},
-        {email}
-     ]
-   });
+    const isUserAlreadyExist = await userModel.findOne({
+      $or: [{ username }, { email }],
+    });
 
-   if(isUserAlreadyExist){
+    if (isUserAlreadyExist) {
       return res.status(409).json({
-        message : "User already exist!"
-      })
-   };
-
-   const hash = await bcrypt.hash(password , 10);
-
-   const user = await userModel.create({
-     username,
-     email,
-     password : hash,
-     role
-   });
-
-   const token = jwt.sign({
-     id : user._id,
-     role: user.role
-   }, process.env.JWT_SECRET);
-
-    res.cookie('token',token);
-
-
-   res.status(201).json({
-    message : "User created successfully",
-    user
-   });
-
-    } catch (error) {
-        console.log("Error in register user" ,error)
-    }
-};
-
-async function loginUser(req,res){
-
-    try {
-        const {username ,email ,password} = req.body;
-
-    const user = await userModel.findOne({
-        $or: [
-            {username},
-            {email}
-        ]
-    })
-
-    if(!user){
-        return res.status(401).json({
-            message : "invalid credentials"
-        })
+        message: "User already exist!",
+      });
     }
 
-    const isPasswordValid = await bcrypt.compare(password , user.password);
+    const hash = await bcrypt.hash(password, 10);
 
-    if(!isPasswordValid){
-        return res.status(401).json({
-            message : "Invalid credentials"
-        })
-    }
+    const user = await userModel.create({
+      username,
+      email,
+      password: hash,
+      role,
+    });
 
-    const token = jwt.sign({
-        id : user._id,
-        role : user.role
-    }, process.env.JWT_SECRET);
+    const token = jwt.sign(
+      {
+        id: user._id,
+        role: user.role,
+      },
+      process.env.JWT_SECRET,
+    );
 
-    res.cookie('token',token);
+    res.cookie("token", token, {
+      httpOnly: true,
+      sameSite: "None",
+      secure: true,
+    });
 
     res.status(201).json({
-        message : "User loggedin successfully",
-        user : {
-            id : user._id,
-            username : user.username,
-            email : user.email,
-            role : user.role
-        }
+      message: "User created successfully",
+      user,
     });
-    } catch (error) {
-        console.log("Error in login user" ,error);
-        
-    }
-};
+  } catch (error) {
+    console.log("Error in register user", error);
+  }
+}
 
-module.exports= {registerUser , loginUser}
+async function loginUser(req, res) {
+  try {
+    const { username, email, password } = req.body;
+
+    const user = await userModel.findOne({
+      $or: [{ username }, { email }],
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        message: "invalid credentials",
+      });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        role: user.role,
+      },
+      process.env.JWT_SECRET,
+    );
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      sameSite: "None",
+      secure: true,
+    });
+
+    res.status(201).json({
+      message: "User loggedin successfully",
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    console.log("Error in login user", error);
+  }
+}
+
+module.exports = { registerUser, loginUser };
